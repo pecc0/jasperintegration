a83eb9f643f58deb01f7e666ef8952ae3b2ec6b1
air_biker@abv.bg <air_biker@abv.bg@a9061cf8-1d79-11df-9040-c31751e811b8>
2010-02-19 20:44:39 +0000

    Libs all working

--- src/bi/jasperIntegration/ReportsMain.java
+++ src/bi/jasperIntegration/ReportsMain.java
@@ -1,22 +1,46 @@
 package bi.jasperIntegration;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import bi.jasperIntegration.groovy.ReportsManager;
 
 public class ReportsMain {
 
 	/**
 	 * TODO - DOCUMENT ME
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		String reportsXml = "reports.xml";
-		
+
 		for (int i = 0; i < args.length; i++) {
 			if ("-reports".equals(args[i])) {
 				reportsXml = args[++i];
 			}
- 		}
-		new ReportsManager().createReports(reportsXml, null);
+		}
+
+		try {
+			Driver d = (Driver) Class.forName("sun.jdbc.odbc.JdbcOdbcDriver")
+					.newInstance();
+			Connection c = DriverManager
+					.getConnection("jdbc:odbc:project1");
+			new ReportsManager().createReports(reportsXml, c);
+			c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	void saveReport(byte[] report, File f) {
+
+		// FileOutputStream fos = new FileOutputStream(f);
+		// fos.write(report);
+	}
 }
--- src/bi/jasperIntegration/groovy/ReportsManager.groovy
+++ src/bi/jasperIntegration/groovy/ReportsManager.groovy
@@ -1,11 +1,13 @@
 package bi.jasperIntegration.groovy
 
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperReport;;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.*;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.engine.JasperPrint;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.sql.Connection;
 
 import groovy.util.slurpersupport.GPathResult;
@@ -19,15 +21,68 @@ class ReportsManager {
 		
 		directory = reports.'@reportsDir';
 		println "dir= $directory";
+		String outputDir = reports.'@outputDir';
 		
-		reports.children().each {
+		reports.report.each {
 			//GPathResult report = it;
 			println "name=${it.'@name'}";
-			JasperReport jr = loadReport(it.'@name');
+			def reportName = it.'@name';
 			
+			JasperReport jr = loadReport(reportName);
+			def parameters = [:];
 			it.param.each {
-				println it;
+				println "param ${it.'@key'}=${it.'@value'}";
+				parameters[it.'@key'] = it.'@value';
 			}
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jr, parameters, conn);
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			
+			JRAbstractExporter exporter = null;
+			
+			def reportFormat = it.format.toString();
+			
+			String outputFileName = it.outputName; 
+			if (outputFileName.length() == 0) {
+				outputFileName = reportName
+			}
+			//CSV
+			if("CSV".equalsIgnoreCase(reportFormat)) {
+				println("CSV report exported");
+				String delimiter = (String)it.delimiter;
+				exporter = new JRCsvExporter();
+				exporter.setParameter(JRCsvExporterParameter.FIELD_DELIMITER, delimiter);					
+				//PDF
+			} else if ("PDF".equalsIgnoreCase(reportFormat)) {
+				println("PDF report exported");
+				exporter = new JRPdfExporter();
+				//XLS
+			} else if ("XLS".equalsIgnoreCase(reportFormat)) {
+				println("XLS report exported");
+				exporter = new JRXlsExporter();					
+				//HTML
+			} else if ("HTML".equalsIgnoreCase(reportFormat)) {
+				println("HTML report exported");
+				exporter = new JRHtmlExporter();
+				//TXT
+			} else if ("TXT".equalsIgnoreCase(reportFormat)) {
+				println("TXT report exported");
+				exporter = new JRTextExporter();
+				//RTF
+			} else if ("RTF".equalsIgnoreCase(reportFormat)) {
+				println("RTF report exported");
+				exporter = new JRRtfExporter();
+			}
+			else{
+				println("Error: Format type for " + reportName + " is missing (or not correct):"+reportFormat);
+			}
+			
+			exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);	
+			exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, outputFileName);				
+			exporter.exportReport();	
+			
+			saveReport(baos.toByteArray(), new File(outputDir, "$outputFileName.${reportFormat.toLowerCase()}"))
 		}
 	}
 	
@@ -37,6 +92,12 @@ class ReportsManager {
 		return JasperCompileManager.compileReport(jasperDesign);
 	}
 	
+	void saveReport(byte[] report, File f) {
+		println f;
+		FileOutputStream fos = new FileOutputStream(f);
+		fos.write(report);
+		fos.close();
+	}
 }
 
 
